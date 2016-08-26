3e129199678250012edf06a93971a41e1f981f26
ven3k <admin@ven3k.eu>
2016-08-26 21:36:24 +0200

    poprawiona literowka
    jw.

--- idziennikextended.user.js
+++ idziennikextended.user.js
@@ -17,7 +17,7 @@ iListView_utworzPanel(cTableName); // Dodaj nowy panel z przyciskiem "Nowy"
 console.log('Nowy panel utworzony');
 
 function iListView_newRecord(dz_tableID, isReply, subject){
-	if (iListView_idTable == cTableName) {
+	if (dz_TableID == cTableName) {
 		listaOdbiorcow = new Array();
 		onClickEnabled = ' onclick = "otworzOknoZKontaktamiFull()" ';
 		disabledInput = '';
